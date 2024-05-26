14cd33545b11c410db091f66fbe7e184193e9b1e
pranav <guptapranav2504@gmail.com>
2024-05-26 14:52:06 +0530

    lead

--- test/leadgeneration.py
+++ test/leadgeneration.py
@@ -2,13 +2,14 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 def test_lead():
     driver = webdriver.Chrome()
     driver.get("https://psquickit2-dev-ed.develop.my.salesforce.com/")
 
-    time.sleep(30)
+    time.sleep(10)
 
     username = driver.find_element(By.XPATH, "//input[@id='username']")
     username.send_keys("pranav.hitmantoto@psquickit.com")
@@ -17,6 +18,13 @@ def test_lead():
     submit_button = driver.find_element(By.ID, "Login")
     submit_button.click()
 
-    time.sleep(30)
+    time.sleep(20)
 
     assert "psquickit2" in driver.page_source
+    driver.maximize_window()
+    driver.find_element(By.XPATH, "//div[@class='slds-icon-waffle']/..//div[@class='slds-icon-waffle']").click()
+
+    time.sleep(10)
+    actions = ActionChains(driver)
+    actions.send_keys("Leads").click().perform()
+    time.sleep(30)
