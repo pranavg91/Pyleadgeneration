35efa83deb677f4960ca611405d89e03588b8be4
pranav <guptapranav2504@gmail.com>
2024-05-30 08:54:47 +0530

    30 may

--- test/leadgeneration.py
+++ test/leadgeneration.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 
 def test_lead():
@@ -24,7 +25,17 @@ def test_lead():
     driver.maximize_window()
     driver.find_element(By.XPATH, "//div[@class='slds-icon-waffle']/..//div[@class='slds-icon-waffle']").click()
 
-    time.sleep(10)
-    actions = ActionChains(driver)
-    actions.send_keys("Leads").click().perform()
     time.sleep(30)
+    actions = ActionChains(driver)
+
+    driver.find_element(By.XPATH,"//div[@class='slds-r6']//following::input[@id='input-113']").send_keys("Leads")
+    actions.send_keys(Keys.ARROW_DOWN).send_keys(Keys.ENTER).perform()
+
+    time.sleep(30)
+
+    # click to new button
+
+    driver.find_element(By.XPATH,"//button[@class='slds-button slds-button_neutral slds-button_last toListViewButton']//preceding::button[normalize-space()='New']").click()
+    time.sleep(20)
+
+
